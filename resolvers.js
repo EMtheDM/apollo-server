3f9c021aa3cd3f804874dd8a7e9c66fef58c308f
Eric Martin <eric_martin@me.com>
2022-03-13 22:25:10 -0600

    commit work I have complete so far

---
+++ resolvers.js
@@ -0,0 +1,27 @@
+const resolvers = {
+  Query: {
+    async link(root, { id }, { models }) {
+      return models.Link.findByPk(id);
+    },
+    async allLinks(root, args, { models }) {
+      return models.Link.findAll();
+    },
+  },
+
+  Mutation: {
+    async createLink(root, { url, slug }, { models }) {
+      return models.Link.create({
+        url,
+        slug,
+      });
+    },
+  },
+
+  Link: {
+    async links(link) {
+      return link.getLinks();
+    },
+  },
+};
+
+module.exports = resolvers;
